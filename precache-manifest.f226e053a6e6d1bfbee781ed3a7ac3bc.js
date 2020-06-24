463a1454c6ffab4d04836403cc30a46e781a923b
Ganeshnotes <ganesshrgorde@gmail.com>
2020-06-24 18:15:05 +0530

    Updates

---
+++ precache-manifest.f226e053a6e6d1bfbee781ed3a7ac3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c467173bb11899550610d6220e2361",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "cf2bff4a55a83acb1c22",
+    "url": "/store/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1594c770080f34c651f",
+    "url": "/store/static/js/2.b223e576.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/store/static/js/2.b223e576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2bff4a55a83acb1c22",
+    "url": "/store/static/js/main.101ab264.chunk.js"
+  },
+  {
+    "revision": "973c47ba3524bad7313c",
+    "url": "/store/static/js/runtime-main.72c9e998.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/store/static/media/logo.5d5d9eef.svg"
+  }
+]);
